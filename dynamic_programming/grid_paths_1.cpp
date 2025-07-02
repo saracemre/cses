e0a77359daf88857901b2c9e9256ad7d7086975b
Emre Saraç <283126@student.pwr.edu.pl>
2025-07-02 11:25:36 +0300

    grid_paths_1.cpp added.

---
+++ dynamic_programming/grid_paths_1.cpp
@@ -0,0 +1,133 @@
+#include <cassert>
+#include <cctype>
+#include <cerrno>
+#include <cfloat>
+#include <ciso646>
+#include <climits>
+#include <clocale>
+#include <cmath>
+#include <csetjmp>
+#include <csignal>
+#include <cstdarg>
+#include <cstddef>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+
+#include <ccomplex>
+#include <cfenv>
+#include <cinttypes>
+#include <cstdbool>
+#include <cstdint>
+#include <ctgmath>
+#include <cwchar>
+#include <cwctype>
+
+// C++
+#include <algorithm>
+#include <bitset>
+#include <complex>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <typeinfo>
+#include <utility>
+#include <valarray>
+#include <vector>
+
+#include <array>
+#include <atomic>
+#include <chrono>
+#include <condition_variable>
+#include <forward_list>
+#include <future>
+#include <initializer_list>
+#include <mutex>
+#include <random>
+#include <ratio>
+#include <regex>
+#include <scoped_allocator>
+#include <system_error>
+#include <thread>
+#include <tuple>
+#include <typeindex>
+#include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
+
+using namespace std;
+
+#define int long long
+const int MOD = 1e9+7;
+
+void solve() {
+    int n;
+    cin >> n;
+    vector<vector<char>> grid(n, vector<char>(n));
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cin >> grid[i][j];
+        }
+    }
+    
+    vector<vector<int>> dp(n, vector<int>(n));
+    if (grid[0][0] == '*') {
+        cout << 0 << '\n';
+        return;
+    } else {
+        dp[0][0] = 1;
+    }
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (i == 0 && j == 0) {
+                continue;
+            }
+            if (grid[i][j] == '*') {
+                dp[i][j] = 0;
+            } else if (i == 0) {
+                dp[i][j] = dp[i][j-1];
+            } else if (j == 0) {
+                dp[i][j] = dp[i-1][j];
+            } else {
+                dp[i][j] = dp[i-1][j] + dp[i][j-1];
+                dp[i][j] %= MOD;
+            }
+        }
+    }
+    cout << dp[n-1][n-1] << '\n';
+}
+
+signed main () {
+    ios::sync_with_stdio(0); 
+    cin.tie(0); 
+    cout.tie(0);
+
+    int t = 1;
+    // cin >> t;
+    while (t--) solve();
+
+    return 0;
+}
